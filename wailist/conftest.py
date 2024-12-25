b6e261ed32d889021dd80b85e958490ce4c196dd
Aayan Shaikh <82713836+aayan-shaikh@users.noreply.github.com>
2024-12-25 13:26:05 +0530

    init

---
+++ wailist/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wailist.users.models import User
+from wailist.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
